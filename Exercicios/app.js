ee1856d637bb915a67520f558a7ee07e12c0f665
MARCIO TORRES RODRIGUES DE SOUZA <0031432412031@lti.fatecso.edu.br>
2024-10-02 18:03:18 -0300

    02/10/2024

--- Exercicios/app.js
+++ Exercicios/app.js
@@ -1,21 +1,18 @@
-var express = require('express')
-var texto = require('/modulo1.js')
-
-var app = express()
+var app = require('./app/config/server')
 
 app.set('view engine','ejs');
 
 app.get('/', function(req,res){
-    res.sand("<html><body>Home da Fatec Sorocaba!</body></html>")
+    res.send("<html><body>Home da Fatec Sorocaba!</body></html>")
 });
 app.get('/historia', function(req,res){
-    res.sand("<html><body>Historia da Fatec Sorocaba</body></html>")
+    res.send("<html><body>Historia da Fatec Sorocaba</body></html>")
 });
 app.get('/cursos', function(req,res){
-    res.sand("<html><body>Cursos da Fatec Sorocaba</body></html>")
+    res.send("<html><body>Cursos da Fatec Sorocaba</body></html>")
 });
 app.get('/professores', function(req,res){
-    res.sand("<html><body>Professores da Fatec Sorocaba</body></html>")
+    res.send("<html><body>Professores da Fatec Sorocaba</body></html>")
 });
 
 app.get('/informacao/cursos', function(req,res){
@@ -27,5 +24,5 @@ app.get('/informacao/professores', function(req,res){
 });
 
 app.listen(3000, function(){
-    
+    console.log("servidor iniciado")
 });
---
+++ Exercicios/modulo1.js
@@ -0,0 +1,3 @@
+var texto = "Observe que esta mensagem vem do modulo";
+
+module.export = texto;
